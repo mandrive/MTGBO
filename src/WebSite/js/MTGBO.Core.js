d7ce33e9202a304787b0c0dcd65f43468d7541e9
mandrive <maciek.kozera@gmail.com>
2014-08-03 17:30:32 +0200

    added cards list pagination and name filtering

--- src/WebSite/js/MTGBO.Core.js
+++ src/WebSite/js/MTGBO.Core.js
@@ -1,109 +1,136 @@
 /// <reference path="./angular.min.js" />
 
-var MtgboApp = angular.module('MtgboApp', ['ngRoute', 'ngResource']);
+var MtgboApp = angular.module('MtgboApp', ['ngRoute', 'ngResource', 'ui.bootstrap'])
 
-MtgboApp.config(['$routeProvider',
-function($routeProvider) {
-	$routeProvider.when('/', {
-	   templateUrl : 'home.html',
-		controller : 'HomeController'
-    }).when('/home', {
-		templateUrl : 'home.html',
-		controller : 'HomeController'
-	}).when('/logon', {
-		templateUrl : 'logon.html',
-		controller : 'LogonController'
-	}).otherwise({
-		redirectTo : '/logon'
-	});
-}]);
+.config(['$routeProvider',
+function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        }).when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        }).when('/logon', {
+            templateUrl: 'logon.html',
+            controller: 'LogonController'
+        }).otherwise({
+            redirectTo: '/logon'
+        });
+}])
 
-MtgboApp.controller('MtgboAppController', ['$scope', '$location', '$rootScope', function($scope, $location, $rootScope) { 
-    $rootScope.setCurrentUser = function(user){
-        $rootScope.currentUser = user;
-    };
-    
-    $rootScope.$on('$routeChangeStart', function(scope, next, current) {
+.controller('MtgboAppController', ['$scope', '$location', '$rootScope',
+    function ($scope, $location, $rootScope) {
+        $rootScope.setCurrentUser = function (user) {
+            $rootScope.currentUser = user;
+        };
+
+        /*$rootScope.$on('$routeChangeStart', function(scope, next, current) {
         if ($rootScope.currentUser) {
         } else {
             $location.path('/logon');
         }
-    });
-    
-    $scope.isUserLogged = function()
-    {
-        if($rootScope.currentUser) return true;
-        return false;
-    }
-    
-    $rootScope.loadAllCards = function() {
-        $.getJSON('data/AllSets.json', function(data) {         
-            $rootScope.allSets = data;
-            $rootScope.allCards = [];
-            for(var set in data) {
-                $rootScope.allCards = $rootScope.allCards.concat(data[set].cards);
-            }
-        });
-    }
-    
-    $rootScope.loadAllCards();
-}]);
+    });*/
 
-MtgboApp.controller('LogonController', ['$scope', '$rootScope', '$location', 'AuthService', function($scope, $rootScope, $location, AuthService) {
-  $scope.credentials = {
-    username: '',
-    password: ''
-  };
-
-  $scope.login = function (credentials) {
-      if (credentials.username && credentials.password)
-      {
-        // mock only
-        if (credentials.username == "test" && credentials.password == "test")
-        {
-            $rootScope.setCurrentUser(credentials);
-            $location.path('/home');
+        $scope.isUserLogged = function () {
+            if ($rootScope.currentUser) return true;
+            return false;
         }
-      }
-  };
-}]);
+}])
 
-MtgboApp.controller('HomeController', ['$scope', '$rootScope', function($scope, $rootScope) {
-    /*$scope.addNewEventWindow = initializeDialogs();
-    $rootScope.fullCalendar = $('#calendar').data("fullCalendar").getCalendar();
-    var addEvent = function() {
-        $rootScope.fullCalendar.addEventSource([{title: 'event2', start: '2014-07-29'}]);
-    };
-    
-    var bindDayClickEvent = function() {
-        $rootScope.fullCalendar.options.dayClick = function() {$scope.addNewEventWindow.dialog("open");}
-    };
-    
-    addEvent();
-    bindDayClickEvent();*/
-}]);
+.controller('LogonController', ['$scope', '$rootScope', '$location', 'AuthService',
+    function ($scope, $rootScope, $location, AuthService) {
+        $scope.credentials = {
+            username: '',
+            password: ''
+        };
 
-MtgboApp.factory('AuthService', function() {
-    this.login = function(credentials) {
-    }
+        $scope.login = function (credentials) {
+            if (credentials.username && credentials.password) {
+                // mock only
+                if (credentials.username == "test" && credentials.password == "test") {
+                    $rootScope.setCurrentUser(credentials);
+                    $location.path('/home');
+                }
+            }
+        };
+}])
+
+.controller('HomeController', ['$scope', '$rootScope', 'CardsService',
+    function ($scope, $rootScope, CardsService) {
+
+        // pagination
+        $scope.pageChanged = function () {
+            $scope.prepareCardsSample();
+        };
+
+        $scope.allSets = null;
+        $scope.allCards = [];
+        $scope.cardsPart = [];
+        $scope.itemsPerPage = 10;
+        $scope.currentPage = 1;
+
+        $scope.filterAllCards = function (input) {
+            $scope.allCardsFiltered = $scope.allCards.filter(function (element) {
+                return element.name.toLowerCase().indexOf(input.toLowerCase()) > -1;
+            });
+        };
+
+        $scope.prepareCardsSample = function () {
+            var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+            var end = begin + $scope.itemsPerPage;
+            $scope.cardsPart = $scope.allCardsFiltered.slice(begin, end);
+        };
+
+        var cardsPromise = CardsService.getCards();
+        cardsPromise.then(function (data) {
+            $scope.allSets = data;
+            $scope.allCards = [];
+            for (var set in data) {
+                $scope.allCards = $scope.allCards.concat(data[set].cards);
+                $scope.allCardsFiltered = $scope.allCards;
+            }
+            $scope.prepareCardsSample();
+        });
+
+        $scope.$watch('searchCardName', function (term) {
+            // Create $scope.filtered and then calculat $scope.noOfPages, no racing!
+            $scope.filterAllCards(term); // $scope.filtered = filterFilter($scope.list, term);
+            //$scope.noOfPages = Math.ceil($scope.filtered.length/$scope.entryLimit);
+            $scope.prepareCardsSample();
+        });
+}])
+
+.service('CardsService', function ($http, $q) {
+    var deferred = $q.defer();
+    $.getJSON('data/AllSets.json', function (data) {
+        var i = 0;
+        i++;
+        deferred.resolve(data);
+    });
+    this.getCards = function () {
+        return deferred.promise;
+    };
+})
+
+.factory('AuthService', function () {
+    this.login = function (credentials) {}
 });
 
 function initializeDialogs() {
-var dialog = $( "#eventForm" ).dialog({
-      autoOpen: false,
-      height: 300,
-      width: 350,
-      modal: true,
-      buttons: {
-        "Add new event": function() { return true; },
-        Cancel: function() {
-          dialog.dialog( "close" );
-        }
-      },
-      close: function() {
-        //form[ 0 ].reset();
-        //allFields.removeClass( "ui-state-error" );
-      }
+    var dialog = $("#eventForm").dialog({
+        autoOpen: false,
+        height: 300,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Add new event": function () {
+                return true;
+            },
+            Cancel: function () {
+                dialog.dialog("close");
+            }
+        },
+        close: function () {}
     });
     return dialog;
 }
